43fb4d1b5c576b1cb9f0d476cea9d636b989bec5
HninNweAye <hninnwe.coder@gmail.com>
2019-04-02 15:33:42 +0800

    For Zin LoginPart

---
+++ ShoppingCartCA/DB/ProductsData.cs
@@ -0,0 +1,23 @@
+﻿using ShoppingCartCA.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace ShoppingCartCA.DB
+{
+    public class ProductsData
+    {
+        public static Products GetProductsByName(string username)
+        {
+            Products products = null;
+
+            using (SqlConnection conn = new SqlConnection(Data.connectionString))
+            {
+                conn.Open();
+            }
+            return products;
+        }
+    }
+}
---
+++ ShoppingCartCA/DB/UsersData.cs
@@ -0,0 +1,23 @@
+﻿using ShoppingCartCA.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace ShoppingCartCA.DB
+{
+    public class UsersData
+    {
+        public static Products GetProductsByName(string username)
+        {
+            Products products = null;
+
+            using (SqlConnection conn = new SqlConnection(Data.connectionString))
+            {
+                conn.Open();
+            }
+            return products;
+        }
+    }
+}
