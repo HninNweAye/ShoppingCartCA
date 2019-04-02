26dba9da93d839d53c8c377de94f3aa76859b719
HninNweAye <hninnwe.coder@gmail.com>
2019-04-02 11:27:26 +0800

    User and PurchaseHistory Models

---
+++ ShoppingCartCA/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ShoppingCartCA.Models
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        [Required (ErrorMessage = "UserName is required")]
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
